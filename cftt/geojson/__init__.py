680b128e3319ae5eafd3ac84cfa29dac7dcd9990
osoken <osoken.devel@outlook.jp>
2016-03-09 00:07:06 +0900

    add util.py

--- cftt/geojson/__init__.py
+++ cftt/geojson/__init__.py
@@ -3,6 +3,10 @@
 import sys;
 import os;
 
+sys.path.insert(0, os.path.dirname(__file__));
+
+import util;
+
 import json;
 import urllib2;
 
@@ -14,6 +18,29 @@ from shapely.ops import cascaded_union;
 class feature(object):
   """feature オブジェクト。
   """
+  def __init__(self, d):
+    """feature オブジェクトの初期化
+    :param d: 初期化するためのデータ。load 関数に準ずる
+    """
+    super(feature, self).__init__();
+    self.load(d);
+  def construct(self, geom, prop, attr):
+    """feature オブジェクトを構成する
+    :param geom: shape オブジェクト
+    :param prop: Mapping オブジェクト
+    :param attr: Mapping オブジェクト
+    """
+
+  def load(self, d):
+    """feature オブジェクトを d から構成する
+    :param d: 'geometry', 'properties' を持つMappingオブジェクトかfeatureインスタンス
+    """
+    if isinstance(d, feature):
+      self.__geometry = d.__geometry;
+      self.__attributes = d.__attributes;
+      self.__properties = d.__properties;
+    else:
+
 
 class geoJson(object):
   """geoJson オブジェクト。
---
+++ cftt/util.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+
+import sys;
+import os;
+import collections;
+
+def const(x):
+  """常にxを返す関数を返す
+  :param x: 任意のオブジェクト
+  """
+  return lambda *args,**kwargs: x;
+
+def isMapping(x):
+  """xが辞書系オブジェクトならTrue、そうでなければFalseを返す
+  :param x: 任意のオブジェクト
+  """
+  return isinstance(x, collections.Mapping);
+
+def isString(x):
+  """xがunicode文字列か文字列ならTrue、そうでなければFalseを返す
+  :param x: 任意のオブジェクト
+  """
+  return isinstance(x, basestring);
+
+def isIterable(x):
+  """xがiterableであり、文字列系でない場合True、そうでなければFalseを返す
+  :param x: 任意のオブジェクト
+  """
+  return isinstance(x, collections.Iterable) and not isinstance(x, basestring);
+
+def isReadable(x):
+  """xがread可能なオブジェクトの場合True、そうでなければFalseを返す
+  :param x: 任意のオブジェクト
+  """
+  return hasattr(x, 'read') and hasattr(x.read, '__call__');
+
+def isWritable(x):
+  """xがwrite可能なオブジェクトの場合True、そうでなければFalseを返す
+  :param x: 任意のオブジェクト
+  """
+  return hasattr(x, 'write') and hasattr(x.write, '__call__');
+
+def _print(x):
+  """print 文の関数版。
+  :param x: 任意のオブジェクト
+  """
+  print x;
+
+def _raise(x):
+  """raise 文の関数版。この関数の中から x　をraise する
+  :param x: 任意のオブジェクト
+  """
+  raise x;
+
+def rApply(f, x, condition=const(True), isMapping=isMapping, isIterable=isIterable, applyToKey=True):
+  """xに対して再帰的にfを適用する。conditionで適用するかどうかの判定ができる。
+  :param f: 一つの引数を取る関数
+  :param x: 任意のオブジェクト
+  :param condition: 一つの引数を取り、真偽値を返す関数
+  :param isMapping: 辞書系オブジェクトかどうか判定する関数
+  :param isIterable:　イテレータオブジェクトかどうか判定する
+  :param applyToKey: 辞書系オブジェクトのkeyにも関数を適用するかどうかの真偽値
+  """
+  if isMapping(x):
+    if applyToKey:
+      try:
+        return x.__class__( ( (rApply(f,k,condition,isMapping,isIterable,applyToKey),rApply(f,v,condition,isMapping,isIterable,applyToKey)) for k,v in x.items() ) );
+      except:
+        return dict( ( (rApply(f,k,condition,isMapping,isIterable,applyToKey),rApply(f,v,condition,isMapping,isIterable,applyToKey)) for k,v in x.items() ) );
+    try:
+      return x.__class__( ( (k,rApply(f,v,condition,isMapping,isIterable,applyToKey)) for k,v in x.items() ) );
+    except:
+      return dict( ( (k,rApply(f,v,condition,isMapping,isIterable,applyToKey)) for k,v in x.items() ) );
+  if isIterable(x):
+    try:
+      return x.__class__( ( rApply(f,v,condition,isMapping,isIterable,applyToKey) for v in x ) );
+    except:
+      return [ rApply(f,v,condition,isMapping,isIterable,applyToKey) for v in x ];
+  if condition(x):
+    return f(x);
+  return x;

bcd889663a81722361838aea1d62e91278f1e1dd
osoken <osoken.devel@outlook.jp>
2016-03-10 22:08:24 +0900

    lint pep8 	modified:   .editorconfig 	modified:   cftt/common/__init__.py 	modified:   cftt/geojson/manip.py 	modified:   cftt/util.py

--- cftt/geojson/manip.py
+++ cftt/geojson/manip.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 
-import sys;
-import os;
+import sys
+import os
 
-import json;
+import json
 
-import collections;
+import collections
 
-from shapely.geometry import shape, mapping;
-from shapely.ops import cascaded_union;
+from shapely.geometry import shape, mapping
+from shapely.ops import cascaded_union
 
-def recApply(d, func, condition=lambda x:True, isMap=lambda x:isinstance(x,collections.Mapping), isContainer=lambda x:isinstance(x,collections.Iterable) and not isinstance(x, basestring), applyToKey=True):
+def recApply(d, func, condition=lambda x: True, isMap=lambda x: isinstance(x, collections.Mapping), isContainer=lambda x: isinstance(x, collections.Iterable) and not isinstance(x, basestring), applyToKey=True):
   """d に対して func を再帰的に適用する
 
   :param d: 任意の変数
@@ -22,27 +22,27 @@ def recApply(d, func, condition=lambda x:True, isMap=lambda x:isinstance(x,colle
   """
   if isMap(d):
     if applyToKey:
-      return d.__class__( ( (recApply(k,func,condition,isMap,isContainer,applyToKey),recApply(v,func,condition,isMap,isContainer,applyToKey)) for k,v in d.items() ) );
-    return d.__class__( ( (k,recApply(v,func,condition,isMap,isContainer,applyToKey)) for k,v in d.items() ) );
+      return d.__class__( ( (recApply(k,func,condition,isMap,isContainer,applyToKey),recApply(v,func,condition,isMap,isContainer,applyToKey)) for k,v in d.items() ) )
+    return d.__class__( ( (k,recApply(v,func,condition,isMap,isContainer,applyToKey)) for k,v in d.items() ) )
   if isContainer(d):
-    return d.__class__( ( recApply(v,func,condition,isMap,isContainer,applyToKey) for v in d ) );
+    return d.__class__( ( recApply(v,func,condition,isMap,isContainer,applyToKey) for v in d ) )
   if condition(d):
-    return func(d);
-  return d;
+    return func(d)
+  return d
 
 def recDecode(d):
   """d と同じ構造で、 d の全ての str 要素に対して .decode('utf-8') を掛けたものを返す
 
   :param d: 任意の変数
   """
-  return recApply(d, lambda x:x.decode('utf-8'), lambda x:isinstance(x, str));
+  return recApply(d, lambda x:x.decode('utf-8'), lambda x:isinstance(x, str))
 
 def recEncode(d):
   """d と同じ構造で、 d の全ての unicode 要素に対して .encode('utf-8') を掛けたものを返す
 
   :param d: 任意の変数
   """
-  return recApply(d, lambda x:x.encode('utf-8'), lambda x:isinstance(x, unicode));
+  return recApply(d, lambda x:x.encode('utf-8'), lambda x:isinstance(x, unicode))
 
 class feature(object):
   """単一の feature を扱うためのクラス
@@ -52,71 +52,71 @@ class feature(object):
 
     :param data: 'geometry' と 'properties' を属性に持った Mapping オブジェクト
     """
-    self.load(data);
+    self.load(data)
   def load(self, data):
     """data を元に feature を構成する
 
     :param data: 'geometry' と 'properties' を属性に持った Mapping オブジェクト
     """
-    self.__geometry = shape(data['geometry']);
-    self.__properties = recApply(data['properties'],lambda x:x.decode('utf-8'),lambda x:isinstance(x,str));
-    self.__attributes = recApply({k:v for k,v in data.items() if k not in set(('geometry','properties','type'))},lambda x:x.decode('utf-8'),lambda x:isinstance(x,str));
-    return self;
+    self.__geometry = shape(data['geometry'])
+    self.__properties = recApply(data['properties'],lambda x:x.decode('utf-8'),lambda x:isinstance(x,str))
+    self.__attributes = recApply({k:v for k,v in data.items() if k not in set(('geometry','properties','type'))},lambda x:x.decode('utf-8'),lambda x:isinstance(x,str))
+    return self
   def dump(self):
     """このインスタンスを表す、json.dumpsなどでダンプ可能なオブジェクトを返す
     """
-    return dict({'type':'Feature','geometry':mapping(self.__geometry), 'properties':recApply(self.__properties,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode))},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)));
+    return dict({'type':'Feature','geometry':mapping(self.__geometry), 'properties':recApply(self.__properties,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode))},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)))
   def join(self, d):
     """d のキーヴァリューの組をこのインスタンスの properties に追加する
 
     :param d: Mapping オブジェクト
     """
-    self.__properties = dict(self.__properties,**d);
-    return self;
+    self.__properties = dict(self.__properties,**d)
+    return self
   @property
   def properties(self):
     """このインスタンスの properties オブジェクト自体を返す
     """
-    return self.__properties;
+    return self.__properties
   @property
   def geometry(self):
     """このインスタンスの geometry オブジェクト自体を返す
     """
-    return self.__geometry;
+    return self.__geometry
   @property
   def attributes(self):
     """このインスタンスのその他の属性の Mapping オブジェクトを返す
     """
-    return self.__attributes;
+    return self.__attributes
   def isMatch(self, condition):
     """このインスタンスの properties が condition に一致しているかどうか返す
 
     :param condition: 一つの引数（propertiesが渡される）を取る関数
     """
-    return condition(self.__properties);
+    return condition(self.__properties)
 
 class geoJson(object):
   def __init__(self, data):
-    self.__features = [];
-    self.__attributes = {};
+    self.__features = []
+    self.__attributes = {}
     if isinstance(data, basestring):
       with open(data,'r') as f:
-        self.load(json.loads(f.read()));
+        self.load(json.loads(f.read()))
     elif hasattr(data, 'read'):
-      self.load(json.loads(data.read()));
+      self.load(json.loads(data.read()))
     else:
-      self.load(data);
+      self.load(data)
   def load(self, data):
-    self.__attributes = recApply({k:v for k,v in data.items() if k not in set(('type','features'))},lambda x:x.decode('utf-8'),lambda x:isinstance(x,str));
-    self.__features = features(data['features']);
-    return self;
+    self.__attributes = recApply({k:v for k,v in data.items() if k not in set(('type','features'))},lambda x:x.decode('utf-8'),lambda x:isinstance(x,str))
+    self.__features = features(data['features'])
+    return self
   def dump(self):
     if self.__features is None:
-      return dict({'type': 'FeaturesCollection','features':[]},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)));
-    return dict({'type': 'FeaturesCollection','features':self.__features.dump()},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)));
+      return dict({'type': 'FeaturesCollection','features':[]},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)))
+    return dict({'type': 'FeaturesCollection','features':self.__features.dump()},**recApply(self.__attributes,lambda x:x.encode('utf-8'),lambda x:isinstance(x,unicode)))
   def __str__(self):
-    return json.dumps(self.dump());
+    return json.dumps(self.dump())
   def __iter__(self):
     if self.__features is None:
-      raise StopIteration;
-    return self.__features.__iter__();
+      raise StopIteration
+    return self.__features.__iter__()
--- cftt/util.py
+++ cftt/util.py
@@ -1,77 +1,79 @@
 # -*- coding: utf-8 -*-
 
-import sys;
-import os;
-import collections;
+import sys
+import os
+import collections
 
-from datetime import datetime;
-import time;
-import re;
+from datetime import datetime
+import time
+import re
 
-import shortuuid;
+import shortuuid
 
-shortuuid.set_alphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
+shortuuid.set_alphabet(
+  'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+)
 
 def _print(x):
   """print 文の関数版。
 
   :param x: 任意のオブジェクト
   """
-  print x;
+  print x
 
 def _raise(x):
   """raise 文の関数版。この関数の中から x　をraise する
 
   :param x: 任意のオブジェクト
   """
-  raise x;
+  raise x
 
 def genId():
   """22文字のUUIDを生成する
   """
-  return shortuuid.uuid();
+  return shortuuid.uuid()
 
 def const(x):
   """常にxを返す関数を返す
 
   :param x: 任意のオブジェクト
   """
-  return lambda *args,**kwargs: x;
+  return lambda *args,**kwargs: x
 
 def isMapping(x):
   """xが辞書系オブジェクトならTrue、そうでなければFalseを返す
 
   :param x: 任意のオブジェクト
   """
-  return isinstance(x, collections.Mapping);
+  return isinstance(x, collections.Mapping)
 
 def isString(x):
   """xがunicode文字列か文字列ならTrue、そうでなければFalseを返す
 
   :param x: 任意のオブジェクト
   """
-  return isinstance(x, basestring);
+  return isinstance(x, basestring)
 
 def isIterable(x):
   """xがiterableであり、文字列系でない場合True、そうでなければFalseを返す
 
   :param x: 任意のオブジェクト
   """
-  return isinstance(x, collections.Iterable) and not isinstance(x, basestring);
+  return isinstance(x, collections.Iterable) and not isinstance(x, basestring)
 
 def isReadable(x):
   """xがread可能なオブジェクトの場合True、そうでなければFalseを返す
 
   :param x: 任意のオブジェクト
   """
-  return hasattr(x, 'read') and hasattr(x.read, '__call__');
+  return hasattr(x, 'read') and hasattr(x.read, '__call__')
 
 def isWritable(x):
   """xがwrite可能なオブジェクトの場合True、そうでなければFalseを返す
 
   :param x: 任意のオブジェクト
   """
-  return hasattr(x, 'write') and hasattr(x.write, '__call__');
+  return hasattr(x, 'write') and hasattr(x.write, '__call__')
 
 def safeEncode(x, encoding='utf-8'):
   """xがunicode文字列だった場合utf-8にエンコードして返す。それ以外はそのまま返す。
@@ -79,8 +81,8 @@ def safeEncode(x, encoding='utf-8'):
   :param x: 任意のオブジェクト
   """
   if isinstance(x, unicode):
-    return x.encode(encoding);
-  return x;
+    return x.encode(encoding)
+  return x
 
 def safeDecode(x, encoding='utf-8'):
   """xがstr文字列だった場合unicodeにデコードして返す。それ以外はそのまま返す。
@@ -88,8 +90,8 @@ def safeDecode(x, encoding='utf-8'):
   :param x: 任意のオブジェクト
   """
   if isinstance(x, str):
-    return x.decode(encoding);
-  return x;
+    return x.decode(encoding)
+  return x
 
 def reconstructSequence(f, x, defaultSequence=list):
   """xの中身にfを掛けて、xと同じ型で再構築する。再構築に失敗した場合defaultSequence型で再構成する。
@@ -98,11 +100,11 @@ def reconstructSequence(f, x, defaultSequence=list):
   :param x: 任意のイテラブル
   :param defaultSequence: イテラブルのコンストラクタ
   """
-  res = tuple( f(y) for y in x );
+  res = tuple( f(y) for y in x )
   try:
-    return x.__class__(res);
+    return x.__class__(res)
   except:
-    return defaultSequence(res);
+    return defaultSequence(res)
 
 def reconstructMapping(f, x, defaultMapping=dict, applyToKey=True):
   """xの中身にfを掛けて、xと同じ型で再構築する。再構築に失敗した場合defaultMapping型で再構成する。
@@ -113,13 +115,13 @@ def reconstructMapping(f, x, defaultMapping=dict, applyToKey=True):
   :param applyToKey: キーにもfを適用するかの真偽値
   """
   if applyToKey:
-    res = tuple( (f(k),f(v)) for k,v in x.items() );
+    res = tuple( (f(k),f(v)) for k,v in x.items() )
   else:
-    res = tuple( (k,f(v)) for k,v in x.items() );
+    res = tuple( (k,f(v)) for k,v in x.items() )
   try:
-    return x.__class__(res);
+    return x.__class__(res)
   except:
-    return defaultMapping(res);
+    return defaultMapping(res)
 
 
 def dt2Ts(dt):
@@ -127,21 +129,21 @@ def dt2Ts(dt):
 
   :param dt: datetime型
   """
-  return int(time.mktime(dt.timetuple())*1000)+(dt.microsecond/1000);
+  return int(time.mktime(dt.timetuple())*1000)+(dt.microsecond/1000)
 
 def ts2Dt(ts):
   """整数で表されたtimestampをdatetime型に変換する
 
   :param ts: 整数
   """
-  return datetime.fromtimestamp( int(ts)/1000 ).replace(microsecond=int(ts)%1000*1000);
+  return datetime.fromtimestamp( int(ts)/1000 ).replace(microsecond=int(ts)%1000*1000)
 
 def isURL(x):
   """xがURLのパターンにマッチすればTrue、そうでなければFalseを返す
 
   :param x: basestring
   """
-  return re.match(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', x) is not None;
+  return re.match(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', x) is not None
 
 def rApply(f, x, condition=const(True), isMapping=isMapping, isIterable=isIterable, applyToKey=True, defaultMapping=dict, defaultSequence=list):
   """xに対して再帰的にfを適用する。conditionで適用するかどうかの判定ができる。
@@ -155,14 +157,14 @@ def rApply(f, x, condition=const(True), isMapping=isMapping, isIterable=isIterab
   :param defaultMapping: 辞書系オブジェクトを構築する際に、元の型が維持できなかった場合に用いられる辞書系オブジェクトのコンストラクタ
   :param defaultSequence: リスト系オブジェクトを構築する際に、元の型が維持できなかった場合に用いられるリストオブジェクトのコンストラクタ
   """
-  g = lambda y: rApply(f, y, condition, isMapping, isIterable, applyToKey, defaultMapping, defaultSequence);
+  g = lambda y: rApply(f, y, condition, isMapping, isIterable, applyToKey, defaultMapping, defaultSequence)
   if isMapping(x):
-    return reconstructMapping(g, x, defaultMapping, applyToKey);
+    return reconstructMapping(g, x, defaultMapping, applyToKey)
   if isIterable(x):
-    return reconstructSequence(g, x, defaultSequence);
+    return reconstructSequence(g, x, defaultSequence)
   if condition(x):
-    return f(x);
-  return x;
+    return f(x)
+  return x
 
 def rDecode(x, encoding='utf-8'):
   """xに対して再帰的にx.decode(encoding)を掛ける
@@ -170,7 +172,7 @@ def rDecode(x, encoding='utf-8'):
   :param x: 任意のオブジェクト
   :param encoding: エンコーディング。デフォルトはutf-8
   """
-  return rApply(lambda y:y.decode(encoding), x, condition=lambda y:isinstance(y,str));
+  return rApply(lambda y:y.decode(encoding), x, condition=lambda y:isinstance(y,str))
 
 def rEncode(x, encoding='utf-8'):
   """xに対して再帰的にx.encode(encoding)を掛ける
@@ -178,7 +180,7 @@ def rEncode(x, encoding='utf-8'):
   :param x: 任意のオブジェクト
   :param encoding: エンコーディング。デフォルトはutf-8
   """
-  return rApply(lambda y:y.encode(encoding), x, condition=lambda y:isinstance(y,unicode));
+  return rApply(lambda y:y.encode(encoding), x, condition=lambda y:isinstance(y,unicode))
 
 def rStr2Dt(x, timeFormat='%Y/%m/%d %H:%M:%S'):
   """xに対して再帰的にdatetime.strptime(x, timeFormat)を掛ける
@@ -188,10 +190,10 @@ def rStr2Dt(x, timeFormat='%Y/%m/%d %H:%M:%S'):
   """
   def f(y):
     try:
-      return datetime.strptime(y, timeFormat);
+      return datetime.strptime(y, timeFormat)
     except:
-      return y;
-  return rApply(f, x, condition=lambda y: isinstance(y, basestring));
+      return y
+  return rApply(f, x, condition=lambda y: isinstance(y, basestring))
 
 def rDt2Str(x, timeFormat='%Y/%m/%d %H:%M:%S'):
   """xに対して再帰的にx.strftime(timeFormat)を掛ける
@@ -199,4 +201,4 @@ def rDt2Str(x, timeFormat='%Y/%m/%d %H:%M:%S'):
   :param x: 任意のオブジェクト
   :param timeFormat: フォーマット。デフォルトは'%Y/%m/%d %H:%M:%S'
   """
-  return rApply(lambda y:y.strftime(timeFormat), x, condition=lambda y:isinstance(y,datetime));
+  return rApply(lambda y:y.strftime(timeFormat), x, condition=lambda y:isinstance(y,datetime))

04f0b06d7bd5c07584eaf2e472d26c890eade823
osoken <osoken.devel@outlook.jp>
2016-03-15 00:56:51 +0900

    add attr method 	renamed:    cftt/shapefile/shapefile.py -> cftt/shapefile/shapeloader.py 	modified:   tests/shapefile/shapefile.py

--- tests/shapefile/shapefile.py
+++ tests/shapefile/shapefile.py
@@ -6,7 +6,7 @@ import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-from cftt.shapefile import shapefile
+from cftt.shapefile import shapeloader
 
 
 class Tester(unittest.TestCase):
@@ -15,7 +15,9 @@ class Tester(unittest.TestCase):
         pass
 
     def test_ShapeLoader(self):
-        s = shapefile.ShapeLoader()('http://osoken.jp/osoken/cft/data/estat/shp_zip/A002005212000DDSWC13101.zip')
+        l = shapeloader.ShapeLoader()
+        l.attr('note', '本データの作成に当たっては、ESRIジャパン株式会社の全国市区町村界データを使用しました。本データの著作権はESRIジャパン株式会社に帰属します。')
+        s = l('http://www.esrij.com/cgi-bin/wp/wp-content/uploads/2015/08/japan_ver80.zip')
         self.assertNotEqual(s, None)
 
 if __name__ == '__main__':

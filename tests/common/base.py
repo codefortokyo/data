3a1ef970c7dca73d20401faedadbecf53a88a263
osoken <osoken.devel@outlook.jp>
2016-03-26 12:38:01 +0900

    add test for common.base

---
+++ tests/common/base.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+import os
+import unittest
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+from cftt.common import base
